d409cd83e505ce6d7c144b08fb13562ae32515f0
Num0Programmer <num0programmer>
2025-07-15 21:40:55 -0700

    Successfully implemented parsing a list of values passed through terminal.

--- examples/ppm.c
+++ examples/ppm.c
@@ -21,6 +21,7 @@ struct Rect {
     size_t pos[2];  // upper left corner of shape
     size_t width;
     size_t height;
+    size_t color[3];  // RGB [0-255]
 };
 
 
@@ -30,6 +31,24 @@ struct Arguments {
     size_t height;
     uint8_t bg_color;
 };
+
+
+void draw_rect(uint8_t* buf, const size_t width, const size_t height, const struct Rect* rect)
+{
+    for (size_t i = rect->pos[1]; i < rect->pos[1] + rect->height && i < height; i += 1)
+    {
+        // printf("Accessing row = %lu\n", i);
+        for (size_t j = rect->pos[0];
+            j < rect->pos[0] + rect->width * N_CHANNELS && j < width * N_CHANNELS;
+            j += N_CHANNELS)
+        {
+            // printf("Accessing col = %lu\n", j);
+            buf[i * (width * N_CHANNELS) + j] = rect->color[0];
+            buf[i * (width * N_CHANNELS) + j + 1] = rect->color[1];
+            buf[i * (width * N_CHANNELS) + j + 2] = rect->color[2];
+        }
+    }
+}
 
 
 int main(int argc, char** argv)
@@ -90,13 +109,9 @@ int main(int argc, char** argv)
         rect->pos[1] = ((size_t*)(value))[1];
         rect->width = ((size_t*)(value))[2];
         rect->height = ((size_t*)(value))[3];
-        printf(
-            "Rect: { x = %lu, y = %lu, width = %lu, height = %lu }\n",
-            rect->pos[0],
-            rect->pos[1],
-            rect->width,
-            rect->height
-        );
+        rect->color[0] = ((size_t*)(value))[4];
+        rect->color[1] = ((size_t*)(value))[5];
+        rect->color[2] = ((size_t*)(value))[6];
     }
 
     if ((fh = fopen(args.out, "w")) != NULL)
@@ -105,11 +120,12 @@ int main(int argc, char** argv)
         fprintf(fh, "%lu %lu\n", args.width, args.height);
         fprintf(fh, "%u\n", 255);
         buf_size = args.width * args.height * N_CHANNELS;
-        ppm_buf = malloc(sizeof(char) * buf_size);
+        ppm_buf = malloc(sizeof(uint8_t) * buf_size);
         for (size_t i = 0; i < buf_size; i += 1)
         {
             ppm_buf[i] = args.bg_color;
         }
+        if (rect != NULL) draw_rect(ppm_buf, args.width, args.height, rect);
         for (size_t i = 0; i < buf_size; i += 1)
         {
             fprintf(fh, "%u\n", ppm_buf[i]);
